53e66fcee5fec9fbe244651026e9eb29c99d39a1
ics-sawada-h <haruka.sawada@ics-web.jp>
2025-04-09 18:38:26 +0900

    HashRouterに変更

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import { Route, HashRouter as Router, Routes } from "react-router-dom";
 import MainPage from "./pages/MainPage.tsx";
 import PointerPage from "./pages/PointerPage.tsx";
 import HomePage from "./pages/HomePage.tsx";

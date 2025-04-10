393668786117d04b2712a408f1b3830fd87fc50a
ics-sawada-h <haruka.sawada@ics-web.jp>
2025-04-10 12:13:38 +0900

    dreiのサンプル更新(iframe埋め込みのためリンクを削除)

--- src/pages/DreiSamplePage.tsx
+++ src/pages/DreiSamplePage.tsx
@@ -1,58 +1,54 @@
 import { Canvas } from "@react-three/fiber";
 import { Ring } from "../components/Ring.tsx";
 import { CameraControls, Sky, Wireframe } from "@react-three/drei";
-import { Link } from "react-router-dom";
 
 const DreiSamplePage = () => {
   return (
-    <div>
-      <div className="canvasContainer canvasContainer__withLink">
-        <Canvas
-          camera={{
-            fov: 45, // 視野角
-            position: [0, 3, 10], // 位置
-          }}
-        >
-          {/* 背景 */}
-          <Sky
-            distance={4000}
-            sunPosition={[1, 1, 0]}
-            turbidity={0.5}
-            rayleigh={0.1}
-          />
+    <div className="canvasContainer">
+      <Canvas
+        camera={{
+          fov: 45, // 視野角
+          position: [0, 3, 10], // 位置
+        }}
+      >
+        {/* 背景 */}
+        <Sky
+          distance={4000}
+          sunPosition={[1, 1, 0]}
+          turbidity={0.5}
+          rayleigh={0.1}
+        />
 
-          <mesh scale={0.5}>
-            <sphereGeometry />
-            <meshPhongMaterial color={"#1fa6f8"} />
-            {/* consoleにインデックス化されたジオメトリに対してのエラーが出ますが、動作に影響はないので無視することにしています */}
-            <Wireframe stroke={"#ffffff"} thickness={0.1} simplify={true} />
-          </mesh>
+        <mesh scale={0.5}>
+          <sphereGeometry />
+          <meshPhongMaterial color={"#1fa6f8"} />
+          {/* consoleにインデックス化されたジオメトリに対してのエラーが出ますが、動作に影響はないので無視することにしています */}
+          <Wireframe stroke={"#ffffff"} thickness={0.1} simplify={true} />
+        </mesh>
 
-          {/* 飾りの輪っか */}
-          <Ring
-            rotation={[1, 0, 0]}
-            torusArgs={[2.3, 0.15, 16, 100]}
-            meshRotation={{ x: 0.001, y: 0.005, z: 0 }}
-          />
-          <Ring
-            rotation={[0, 1, 0]}
-            torusArgs={[2, 0.1, 16, 100]}
-            meshRotation={{ x: -0.01, y: 0.005, z: 0 }}
-          />
-          <Ring
-            rotation={[0.2, 0.5, 0]}
-            torusArgs={[1.8, 0.02, 16, 100]}
-            meshRotation={{ x: 0.01, y: 0.01, z: 0 }}
-          />
+        {/* 飾りの輪っか */}
+        <Ring
+          rotation={[1, 0, 0]}
+          torusArgs={[2.3, 0.15, 16, 100]}
+          meshRotation={{ x: 0.001, y: 0.005, z: 0 }}
+        />
+        <Ring
+          rotation={[0, 1, 0]}
+          torusArgs={[2, 0.1, 16, 100]}
+          meshRotation={{ x: -0.01, y: 0.005, z: 0 }}
+        />
+        <Ring
+          rotation={[0.2, 0.5, 0]}
+          torusArgs={[1.8, 0.02, 16, 100]}
+          meshRotation={{ x: 0.01, y: 0.01, z: 0 }}
+        />
 
-          <pointLight color={"#b6fbfb"} intensity={100} position={[6, 5, -2]} />
-          <ambientLight intensity={1} />
+        <pointLight color={"#b6fbfb"} intensity={100} position={[6, 5, -2]} />
+        <ambientLight intensity={1} />
 
-          {/* ドラッグやスクロールでカメラを操作できるコントローラー */}
-          <CameraControls makeDefault />
-        </Canvas>
-        <Link to="/">ホームへ戻る</Link>
-      </div>
+        {/* ドラッグやスクロールでカメラを操作できるコントローラー */}
+        <CameraControls makeDefault />
+      </Canvas>
     </div>
   );
 };

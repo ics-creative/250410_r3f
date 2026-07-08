00356f6802a42ddb0960f2ed5f96de5fc7f1ffec
ics-ikeda <ikeda@ics-web.jp>
2026-07-08 13:51:25 +0900

    Vite設定を簡略化

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
-  const basePath = mode === "production" ? "/250410_r3f/" : "./";
-
-  return {
-    plugins: [react()],
-    base: basePath,
-    build: {
-      outDir: "dist",
-    },
-  };
+export default defineConfig({
+  plugins: [react()],
+  base: "./",
 });
